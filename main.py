8bce686289b59381426b9a1c2f8d926eccc5ce41
Avishek Sen <x0rzavi@gmail.com>
2023-11-17 22:20:24 +0530

    add screen scrolling prototype

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+# TODO
+# [x] Scrolling prototype
+# [] Optimization + better implementation
+# [] Typing simulation w/ speed
+# [] Single line printing
+# [] Multi line printing
+# [] Config file
+# [] Scriptable input file
+
 from PIL import Image, ImageDraw, ImageFont
 
 width, height = 640, 480  # VGA
@@ -18,59 +27,77 @@ def linesNumCalc(height: int, yPadding: int, fontHeight: int, lineSpacing: int) 
     return linesNum
 
 linesNum = linesNumCalc(height, yPadding, fontHeight, lineSpacing)
+print(linesNum)
  
+def cursorToLine(lineNum: int) -> None:
+    global xPos, yPos
+    if lineNum > linesNum:
+        return 0
+    else:
+        xPos = xPadding
+        yPos = yPadding + (lineNum - 1) * (fontHeight + lineSpacing)
+        return 1
+
 def genFrame(frame: Image.Image = None, count: int = 1) -> Image.Image:
     global frameCount
     if frame is None:
-        frame = Image.new("RGBA", (width, height), color=bgColor)
+        frame = Image.new("RGBA", (width, height), bgColor)
     for _ in range(count):
         frameCount += 1
         fileName = baseName + str(frameCount) + ".png"
         frame.save(fileName, "PNG")
     return frame
 
-def genText(frame: Image.Image, text: str, x: int, y: int, count: int = 1) -> None:
+def genText(frame: Image.Image, text: str, lineNum: int, count: int = 1) -> None:
     global xPos, yPos
-    pos = (x, y)
-    draw = ImageDraw.Draw(frame)
-    draw.text(pos, text, txtColor, font)
-    bbox = draw.textbbox(pos, text, font);
-    xPos, yPos = bbox[2], bbox[3]
-    yPos += lineSpacing
-    draw.point((xPos, yPos), 'turquoise')
-    frame = genFrame(frame, count)
-    print('x: {} y: {}'.format(xPos, yPos))
+    if cursorToLine(lineNum):
+        pos = (xPos, yPos)
+        draw = ImageDraw.Draw(frame)
+        draw.text(pos, text, txtColor, font)
+        bbox = draw.textbbox(pos, text, font);
+        xPos, yPos = bbox[2], bbox[3]
+        yPos += lineSpacing
+        frame = genFrame(frame, count)
 
+        # debug
+        draw.point((xPos, yPos), 'turquoise')
+        print('x: {} y: {}'.format(xPos, yPos))
+    else:
+        print('Out of Bounds')
+
+def scrollUp(frame: Image.Image) -> Image.Image:
+    croppedFrame = frame.crop((0, fontHeight + lineSpacing, width, height))
+    frame = Image.new("RGBA", (width, height), bgColor)
+    frame.paste(croppedFrame, (0, 0))
+    return frame
+    
 frame = genFrame(None)
-xPos, yPos = 15, 15
+genText(frame, 'Hello first line', 1)
+genText(frame, 'Hello second line', 2)
+genText(frame, 'Hello second last line', 24)
+genText(frame, 'Hello last line', 25)
 
+# croppedImage = frame.crop((0, fontHeight + lineSpacing, width, height))
+# croppedImage.save('frame_cropped.png')
+# newImage = Image.new("RGBA", (width, height), bgColor)
+# newImage.paste(croppedImage, (0, 0))
+# newImage.save('frame_final.png')
+
+frame = scrollUp(frame)
+genText(frame, 'Hello again last line', 25)
+frame = scrollUp(frame)
+genText(frame, 'Hello again last line', 25)
+frame = scrollUp(frame)
+genText(frame, 'Hello again last line', 25)
+
+# Typing Prototype
 # genText(frame, 'Enter username: ', xPos, yPos)
 # for char in 'x0rzavi':
 #     genText(frame, char, xPos, 15)
-
-for i in range(linesNum):
-   genText(frame, 'line ' + str(i), 15, yPos)
+# for i in range(linesNum):
+#    genText(frame, 'line ' + str(i), 15, yPos)
 
 # genText(frame, 'hello', 15, 15)
 # genText(frame, 'coded', xPos, 15)
 # genText(frame, 'by', xPos, 15)
 # genText(frame, 'x0rzavi', 15, yPos)
-
-# frame = genFrame(frame)
-# print(frame)
-# img = Image.new('RGBA', (width, height), color=bgColor)
-# img.save(fileName + str(count) + '.png', 'PNG'); count += 1
-# draw = ImageDraw.Draw(img)
-# font = ImageFont.load(fontFile)
-# fontHeight = draw.textbbox((0, 0), 'test', font=font)[3]
-#
-# j = 0
-# for i in range(5):
-#    img = Image.open(fileName + str(count - 1) + '.png')
-#    draw = ImageDraw.Draw(img)
-#    draw.text((15, j + fontHeight), text, fill=txtColor, font=font)
-#    bbox = draw.textbbox((15, j + fontHeight), text, font=font)
-#    draw.rectangle(bbox, outline='turquoise')
-#    j += fontHeight + 4
-#    print(bbox)
-#    img.save(fileName + str(count) + '.png', 'PNG'); count += 1
