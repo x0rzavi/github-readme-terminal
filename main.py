5bd0f3fb43e4e49316af851d2d593a3b539bf0b4
Avishek Sen <x0rzavi@gmail.com>
2023-11-18 10:04:51 +0530

    add multiple prototypes

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 # TODO
 # [x] Scrolling prototype
+# [] Scrolling
+# [x] Delete lines prototype
+# [] Delete lines
 # [] Optimization + better implementation
+# [x] Typing prototype
 # [] Typing simulation w/ speed
 # [] Single line printing
 # [] Multi line printing
 # [] Config file
 # [] Scriptable input file
+
+import os  # debug
+
+os.system("rm -fr frame*")  # debug
 
 from PIL import Image, ImageDraw, ImageFont
 
@@ -14,81 +22,100 @@ fontFile = "gohufont-uni-14.pil"
 bgColor = "#181825"
 txtColor = "#cdd6f4"
 baseName = "frame_"
+lineSpacing = 4  # default
+
 frameCount = 0
 xPos, yPos = 0, 0
 xPadding, yPadding = 15, 15
-lineSpacing = 4
 
-font = ImageFont.load(fontFile)
-fontHeight = font.getbbox('')[3]
+font = ImageFont.load(fontFile) # bitmap font
+fontWidth, fontHeight = font.getbbox("W")[2], font.getbbox("H")[3]
 
-def linesNumCalc(height: int, yPadding: int, fontHeight: int, lineSpacing: int) -> int:
-    linesNum = (height - 2 * yPadding) // (fontHeight + lineSpacing)
-    return linesNum
+linesNum = (height - 2 * yPadding) // (fontHeight + lineSpacing)
 
-linesNum = linesNumCalc(height, yPadding, fontHeight, lineSpacing)
-print(linesNum)
- 
-def cursorToLine(lineNum: int) -> None:
+
+def cursorToLine(lineNum: int) -> int:
     global xPos, yPos
     if lineNum > linesNum:
+        print("Out of Bounds")
         return 0
     else:
         xPos = xPadding
         yPos = yPadding + (lineNum - 1) * (fontHeight + lineSpacing)
         return 1
 
+
 def genFrame(frame: Image.Image = None, count: int = 1) -> Image.Image:
     global frameCount
     if frame is None:
         frame = Image.new("RGBA", (width, height), bgColor)
+        return frame
     for _ in range(count):
         frameCount += 1
         fileName = baseName + str(frameCount) + ".png"
         frame.save(fileName, "PNG")
     return frame
 
-def genText(frame: Image.Image, text: str, lineNum: int, count: int = 1) -> None:
-    global xPos, yPos
+
+def genText(text: str, lineNum: int, count: int = 1) -> None:
+    global xPos, yPos, frame
     if cursorToLine(lineNum):
         pos = (xPos, yPos)
         draw = ImageDraw.Draw(frame)
         draw.text(pos, text, txtColor, font)
-        bbox = draw.textbbox(pos, text, font);
+        bbox = draw.textbbox(pos, text, font)
         xPos, yPos = bbox[2], bbox[3]
         yPos += lineSpacing
+        print("x: {} y: {}".format(xPos, yPos))  # debug
+        draw.point((xPos, yPos), "turquoise")  # debug
         frame = genFrame(frame, count)
 
-        # debug
-        draw.point((xPos, yPos), 'turquoise')
-        print('x: {} y: {}'.format(xPos, yPos))
-    else:
-        print('Out of Bounds')
 
-def scrollUp(frame: Image.Image) -> Image.Image:
-    croppedFrame = frame.crop((0, fontHeight + lineSpacing, width, height))
-    frame = Image.new("RGBA", (width, height), bgColor)
-    frame.paste(croppedFrame, (0, 0))
-    return frame
-    
-frame = genFrame(None)
-genText(frame, 'Hello first line', 1)
-genText(frame, 'Hello second line', 2)
-genText(frame, 'Hello second last line', 24)
-genText(frame, 'Hello last line', 25)
+def scrollUp(count: int = 1) -> None:
+    global frame
+    for _ in range(count):
+        croppedFrame = frame.crop((0, fontHeight + lineSpacing, width, height))
+        frame = genFrame(None)
+        frame.paste(croppedFrame, (0, 0))
 
-# croppedImage = frame.crop((0, fontHeight + lineSpacing, width, height))
-# croppedImage.save('frame_cropped.png')
-# newImage = Image.new("RGBA", (width, height), bgColor)
-# newImage.paste(croppedImage, (0, 0))
-# newImage.save('frame_final.png')
 
-frame = scrollUp(frame)
-genText(frame, 'Hello again last line', 25)
-frame = scrollUp(frame)
-genText(frame, 'Hello again last line', 25)
-frame = scrollUp(frame)
-genText(frame, 'Hello again last line', 25)
+def deleteLine(lineNum: int) -> None:
+    global xPos, yPos, frame
+    cursorToLine(lineNum)
+    layerImage = Image.new("RGBA", (width, fontHeight + lineSpacing), bgColor)
+    frame.paste(layerImage, (xPos, yPos))
+
+def genTypingText(text: str, lineNum: int, count: int = 1) -> None:
+    global xPos, yPos, frame
+    if cursorToLine(lineNum):
+        draw = ImageDraw.Draw(frame)
+        yPosConst = yPos
+        for char in text:
+            yPos = yPosConst
+            draw.text((xPos, yPos), char, txtColor, font)
+            bbox = draw.textbbox((xPos, yPos), char, font)
+            xPos, yPos = bbox[2], bbox[3]
+            print("x: {} y: {}".format(xPos, yPos))  # debug
+            draw.point((xPos, yPos), "turquoise")  # debug
+            frame = genFrame(frame, count)
+
+
+frame = genFrame(None) # initial blank frame
+# genText("_", 1, 5)
+# deleteLine(1)
+genTypingText("Hello", 1)
+genText("Hello", 2)
+# genText("Hello 1st line", 1)
+# genText("Hello 13th line", 13)
+# genText("Hello 14th line", 14)
+# deleteLine(13)
+# genText('Hello 25th line', 25)
+# genText('Hello 27th line', 27)
+
+# scrollUp()
+# genText("Hello 25th line", 25)
+# scrollUp()
+# genText("Hello 25th line", 25)
 
 # Typing Prototype
 # genText(frame, 'Enter username: ', xPos, yPos)
