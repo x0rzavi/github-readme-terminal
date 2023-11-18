fbf257d6e934c95b0fd9ad1aa2d6b3d62642a42d
Avishek Sen <x0rzavi@gmail.com>
2023-11-18 13:54:35 +0530

    fix existing prototypes

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 # TODO
-# [x] Scrolling prototype
-# [] Scrolling
+# Prototypes
+# [x] Single line printing prototype
+# [x] Continuous printing prototype
 # [x] Delete lines prototype
-# [] Delete lines
-# [] Optimization + better implementation
 # [x] Typing prototype
+# [x] Scrolling prototype
+# Implementations
+# [] Scrolling
+# [] Delete lines
+# [] Optimization + better code quality
 # [] Typing simulation w/ speed
-# [] Single line printing
 # [] Multi line printing
 # [] Config file
 # [] Scriptable input file
+# [] Remove debug statements
 
 import os  # debug
-
 os.system("rm -fr frame*")  # debug
 
 from PIL import Image, ImageDraw, ImageFont
@@ -32,28 +35,31 @@ font = ImageFont.load(fontFile) # bitmap font
 fontWidth, fontHeight = font.getbbox("W")[2], font.getbbox("H")[3]
 
 linesNum = (height - 2 * yPadding) // (fontHeight + lineSpacing)
+linexPos = {_ + 1:xPadding for _ in range(linesNum)}
+# print(linexPos) # debug
 
-
-def cursorToLine(lineNum: int) -> int:
+def cursorToLine(lineNum: int) -> bool:
     global xPos, yPos
     if lineNum > linesNum:
         print("Out of Bounds")
         return 0
     else:
-        xPos = xPadding
+        xPos = linexPos[lineNum]
         yPos = yPadding + (lineNum - 1) * (fontHeight + lineSpacing)
+        print('moved cursor to line {}'.format(lineNum)) # debug
         return 1
 
 
 def genFrame(frame: Image.Image = None, count: int = 1) -> Image.Image:
     global frameCount
     if frame is None:
-        frame = Image.new("RGBA", (width, height), bgColor)
+        frame = Image.new("RGB", (width, height), bgColor)
         return frame
     for _ in range(count):
         frameCount += 1
         fileName = baseName + str(frameCount) + ".png"
         frame.save(fileName, "PNG")
+    print('generated frame') # debug
     return frame
 
 
@@ -66,10 +72,16 @@ def genText(text: str, lineNum: int, count: int = 1) -> None:
         bbox = draw.textbbox(pos, text, font)
         xPos, yPos = bbox[2], bbox[3]
         yPos += lineSpacing
+        linexPos[lineNum] = xPos
         print("x: {} y: {}".format(xPos, yPos))  # debug
         draw.point((xPos, yPos), "turquoise")  # debug
         frame = genFrame(frame, count)
+    print('generated text at line {}'.format(lineNum)) # debug
 
+def genTypingText(text: str, lineNum: int, count: int = 1) -> None:
+    for char in text:
+        genText(char, lineNum, count)
+    print('generated text at line {}'.format(lineNum)) # debug
 
 def scrollUp(count: int = 1) -> None:
     global frame
@@ -77,54 +89,35 @@ def scrollUp(count: int = 1) -> None:
         croppedFrame = frame.crop((0, fontHeight + lineSpacing, width, height))
         frame = genFrame(None)
         frame.paste(croppedFrame, (0, 0))
+        linexPos[linesNum - _] = xPadding
+    print('scrolled up') # debug
 
 
 def deleteLine(lineNum: int) -> None:
     global xPos, yPos, frame
     cursorToLine(lineNum)
-    layerImage = Image.new("RGBA", (width, fontHeight + lineSpacing), bgColor)
-    frame.paste(layerImage, (xPos, yPos))
+    linexPos[lineNum] = xPadding
+    layerImage = Image.new("RGB", (width, fontHeight + lineSpacing), bgColor)
+    frame.paste(layerImage, (0, yPos))
+    print("x: {} y: {}".format(xPos, yPos))  # debug
+    print('deleted line {}'.format(lineNum)) # debug
 
-def genTypingText(text: str, lineNum: int, count: int = 1) -> None:
-    global xPos, yPos, frame
-    if cursorToLine(lineNum):
-        draw = ImageDraw.Draw(frame)
-        yPosConst = yPos
-        for char in text:
-            yPos = yPosConst
-            draw.text((xPos, yPos), char, txtColor, font)
-            bbox = draw.textbbox((xPos, yPos), char, font)
-            xPos, yPos = bbox[2], bbox[3]
-            print("x: {} y: {}".format(xPos, yPos))  # debug
-            draw.point((xPos, yPos), "turquoise")  # debug
-            frame = genFrame(frame, count)
-
-
+# TEST BED
 frame = genFrame(None) # initial blank frame
-# genText("_", 1, 5)
-# deleteLine(1)
-genTypingText("Hello", 1)
-genText("Hello", 2)
-# genText("Hello 1st line", 1)
-# genText("Hello 13th line", 13)
-# genText("Hello 14th line", 14)
-# deleteLine(13)
-# genText('Hello 25th line', 25)
-# genText('Hello 27th line', 27)
+genText("Enter username: ", 1)
+genTypingText("x0rzavi ", 1)
+genText("Continued text! ", 1)
 
-# scrollUp()
-# genText("Hello 25th line", 25)
-# scrollUp()
-# genText("Hello 25th line", 25)
+genText("Enter password: ", 2)
+genTypingText("*** ", 2)
 
-# Typing Prototype
-# genText(frame, 'Enter username: ', xPos, yPos)
-# for char in 'x0rzavi':
-#     genText(frame, char, xPos, 15)
-# for i in range(linesNum):
-#    genText(frame, 'line ' + str(i), 15, yPos)
+deleteLine(1)
+genText("Will this work? ", 1)
+genText("It worked! ", 1)
 
-# genText(frame, 'hello', 15, 15)
-# genText(frame, 'coded', xPos, 15)
-# genText(frame, 'by', xPos, 15)
-# genText(frame, 'x0rzavi', 15, yPos)
+genText("Hello 24th line", 24)
+genText("Hello 25th line", 25)
+scrollUp(2)
+genText("Hello 25th line", 25)
+
+# os.system("ffmpeg -r 2.0 -i 'frame_%d.png' frames.gif")  # debug
