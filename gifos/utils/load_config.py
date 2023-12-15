493b6bc4d2a410bef449e0c62646d0e1b9cde856
Avishek Sen <x0rzavi@gmail.com>
2023-12-15 22:16:31 +0530

    upgrade config loading logic

--- gifos/utils/load_config.py
+++ gifos/utils/load_config.py
@@ -7,16 +7,24 @@ except ModuleNotFoundError:
 
 
 def load_toml(file_name: str) -> dict:
-    config_file = Path("config") / file_name
-    if config_file.exists():
-        with config_file.open(mode="rb") as fp:
-            return tomllib.load(fp)
-    else:
-        print(f"INFO: config_file {file_name} does not exist")
-        return {}
+    def_config_file = (
+        Path(__file__).parents[2] / "config" / file_name
+    )  # default config path
+    user_config_file = Path.home() / ".config" / "gifos" / file_name  # user config path
+
+    with def_config_file.open(mode="rb") as def_fp:
+        def_config = tomllib.load(def_fp)
+    if user_config_file.exists():
+        with user_config_file.open(mode="rb") as user_fp:
+            user_config = tomllib.load(user_fp)
+            return {**def_config, **user_config}  # override with user config
+    print(f"INFO: default config_file {file_name} loaded")
+    return def_config
 
 
 gifos = load_toml("gifos.toml")
 ansi_escape_colors = load_toml("ansi_escape_colors.toml")
+print(gifos)
+print(ansi_escape_colors)
 
 __all__ = ["gifos", "ansi_escape_colors"]
