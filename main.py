117438ef7de6801cf1401bbca3e3e38ef047997d
Avishek Sen <x0rzavi@gmail.com>
2023-12-16 21:15:33 +0530

    update showcase

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
 import gifos
 
-FONT_FILE_LOGO = "/src/fonts/vtks-blocketo.regular.ttf"
-# FONT_FILE_BITMAP = "fonts/ter-u14n.pil"
-FONT_FILE_BITMAP = "/src/fonts/gohufont-uni-14.pil"
-FONT_FILE_TRUETYPE = "/src/fonts/IosevkaTermNerdFont-Bold.ttf"
+FONT_FILE_LOGO = "./fonts/vtks-blocketo.regular.ttf"
+# FONT_FILE_BITMAP = "./fonts/ter-u14n.pil"
+FONT_FILE_BITMAP = "./fonts/gohufont-uni-14.pil"
+FONT_FILE_TRUETYPE = "./fonts/IosevkaTermNerdFont-Bold.ttf"
+FONT_FILE_MONA = "./fonts/Inversionz.otf"
 
 
 def main():
-    t = gifos.Terminal(640, 480, 15, 15, FONT_FILE_BITMAP, 15)
+    t = gifos.Terminal(800, 500, 15, 15, FONT_FILE_BITMAP, 15)
     t.set_fps(15)
 
     t.gen_text("", 1, count=20)
     t.toggle_show_cursor(False)
     t.gen_text("GIF_OS Modular BIOS v1.0.11", 1)
-    t.gen_text("Copyright (C) 2023, \x1b[91mX0rzAvi Softwares Inc.\x1b[0m", 2)
+    t.gen_text("Copyright (C) 2023, \x1b[31mX0rzAvi Softwares Inc.\x1b[0m", 2)
     t.gen_text("\x1b[94mGitHub Profile ReadMe Terminal, Rev 1011\x1b[0m", 4)
     t.gen_text("Krypton(tm) GIFCPU - 250Hz", 6)
     t.gen_text(
@@ -62,7 +66,10 @@ def main():
     t.toggle_show_cursor(True)
     t.gen_typing_text("*********", 4, contin=True)
     t.toggle_show_cursor(False)
-    t.gen_text("Last login: Sun Dec  13 22:55:39 on tty1", 6)
+    time_now = datetime.now(ZoneInfo("Asia/Kolkata")).strftime(
+        "%a %b %d %I:%M:%S %p %Z %Y"
+    )
+    t.gen_text(f"Last login: {time_now} on tty1", 6)
 
     t.gen_prompt(7, count=5)
     prompt_col = t.curr_col
@@ -79,24 +86,25 @@ def main():
     user_details_lines = f"""
     \x1b[30;104mx0rzavi@GitHub\x1b[0m
     --------------
-    \x1b[96mOS:     \x1b[93mArch/Gentoo Linux, Windows 11, Android 13
-    \x1b[96mHost:   \x1b[93mNetaji Subhash Engineering College \x1b[94m#NSEC
-    \x1b[96mKernel: \x1b[93mComputer Science & Engineering \x1b[94m#CSE
-    \x1b[96mUptime: \x1b[93m{user_age.years} years, {user_age.months} months, {user_age.days} days
-    \x1b[96mIDE:    \x1b[93mneovim, VSCode
-
+    \x1b[96mOS:     \x1b[93mArch/Gentoo Linux, Windows 11, Android 13\x1b[0m
+    \x1b[96mHost:   \x1b[93mNetaji Subhash Engineering College \x1b[94m#NSEC\x1b[0m
+    \x1b[96mKernel: \x1b[93mComputer Science & Engineering \x1b[94m#CSE\x1b[0m
+    \x1b[96mUptime: \x1b[93m{user_age.years} years, {user_age.months} months, {user_age.days} days\x1b[0m
+    \x1b[96mIDE:    \x1b[93mneovim, VSCode\x1b[0m
+    
     \x1b[30;104mContact:\x1b[0m
     --------------
-    \x1b[96mEmail:  \x1b[93mx0rzavi@gmail.com
-    \x1b[96mLinkedIn: \x1b[93mavishek-sen-x0rzavi
-
+    \x1b[96mEmail:      \x1b[93mx0rzavi@gmail.com\x1b[0m
+    \x1b[96mLinkedIn:   \x1b[93mavishek-sen-x0rzavi\x1b[0m
+    
     \x1b[30;104mGitHub Stats:\x1b[0m
     --------------
-    \x1b[96mUser Rating: \x1b[93m{git_user_details.user_rank.level}
-    \x1b[96mTotal Stars Earned: \x1b[93m{git_user_details.total_stargazers}
-    \x1b[96mTotal Commits (2023): \x1b[93m{git_user_details.total_commits_last_year}
-    \x1b[96mTotal PRs: \x1b[93m{git_user_details.total_pull_requests_made}\x1b[0m | \x1b[96mMerged PR%: \x1b[93m{git_user_details.pull_requests_merge_percentage}
-    \x1b[96mTotal Contributions: \x1b[93m{git_user_details.total_repo_contributions}
+    \x1b[96mUser Rating: \x1b[93m{git_user_details.user_rank.level}\x1b[0m
+    \x1b[96mTotal Stars Earned: \x1b[93m{git_user_details.total_stargazers}\x1b[0m
+    \x1b[96mTotal Commits (2023): \x1b[93m{git_user_details.total_commits_last_year}\x1b[0m
+    \x1b[96mTotal PRs: \x1b[93m{git_user_details.total_pull_requests_made}\x1b[0m
+    \x1b[96mMerged PR %: \x1b[93m{git_user_details.pull_requests_merge_percentage}\x1b[0m
+    \x1b[96mTotal Contributions: \x1b[93m{git_user_details.total_repo_contributions}\x1b[0m
     \x1b[96mTop Languages: \x1b[93m{', '.join(top_languages[:5])}\x1b[0m
     """
     t.gen_prompt(1)
@@ -108,14 +116,49 @@ def main():
     t.gen_text("\x1b[92mfetch.sh\x1b[0m", 1, contin=True)
     t.gen_typing_text(" -u x0rzavi", 1, contin=True)
 
+    t.set_font(FONT_FILE_MONA, 16, 0)
+    t.toggle_show_cursor(False)
+    monaLines = r"""
+    \x1b[49m     \x1b[34;44m}}\x1b[49m     \x1b[34;44m}}\x1b[0m
+    \x1b[49m    \x1b[34;44m}}}}\x1b[49m   \x1b[34;44m}}}}\x1b[0m
+    \x1b[49m    \x1b[34;44m}}}}}\x1b[49m \x1b[34;44m}}}}}\x1b[0m
+    \x1b[49m   \x1b[34;44m}}}}}}}}}}}}}\x1b[0m
+    \x1b[49m   \x1b[34;44m}}}}}}}}}}}}}}\x1b[0m
+    \x1b[49m   \x1b[34;44m}}\x1b[97;107m}}}}}}}\x1b[34;44m}}}}}\x1b[0m
+    \x1b[49m  \x1b[34;44m}}\x1b[97;107m}}}}}}}}}}\x1b[34;44m}}}\x1b[0m
+    \x1b[49m  \x1b[34;44m}}\x1b[97;107m}\x1b[34;44m}\x1b[97;107m}}}}}\x1b[34;44m}\x1b[97;107m}}\x1b[34;44m}}}}\x1b[0m
+    \x1b[49m  \x1b[34;44m}\x1b[97;107m}}\x1b[34;44m}\x1b[97;107m}}}}}\x1b[34;44m}\x1b[97;107m}}}\x1b[34;44m}}}\x1b[0m
+    \x1b[34;44m}}}\x1b[97;107m}}}}\x1b[34;44m}}}\x1b[97;107m}}}}}\x1b[34;44m}}}}\x1b[0m
+    \x1b[49m  \x1b[34;44m}\x1b[97;107m}}}}}\x1b[34;44m}}\x1b[97;107m}}}}}\x1b[34;44m}}}\x1b[0m
+    \x1b[49m \x1b[34;44m}}\x1b[97;107m}}}}}}}}}}}}\x1b[34;44m}}}\x1b[0m
+    \x1b[34;44m}\x1b[49m  \x1b[34;44m}}\x1b[97;107m}}}}}}}}\x1b[34;44m}}}\x1b[49m  \x1b[34;44m}\x1b[0m
+    \x1b[49m        \x1b[34;44m}}}}}\x1b[0m
+    \x1b[49m       \x1b[34;44m}}}}}}}\x1b[0m
+    \x1b[49m       \x1b[34;44m}}}}}}}}\x1b[0m
+    \x1b[49m      \x1b[34;44m}}}}}}}}}}\x1b[0m
+    \x1b[49m     \x1b[34;44m}}}}}}}}}}}\x1b[0m
+    \x1b[49m     \x1b[34;44m}}}}}}}}}}}}\x1b[0m
+    \x1b[49m     \x1b[34;44m}}\x1b[49m \x1b[34;44m}}}}}}\x1b[49m \x1b[34;44m}}\x1b[0m
+    \x1b[49m        \x1b[34;44m}}}}}}}\x1b[0m
+    \x1b[49m         \x1b[34;44m}}}\x1b[49m \x1b[34;44m}}\x1b[0m
+    """
+    t.gen_text(monaLines, 10)
+
+    t.set_font(FONT_FILE_BITMAP)
+    t.toggle_show_cursor(True)
     # t.pasteImage("./temp/x0rzavi.jpg", 3, 5, sizeMulti=0.5)
-    t.gen_text(user_details_lines, 2, contin=True)
+    t.gen_text(user_details_lines, 2, 35, count=5, contin=True)
     t.gen_prompt(t.curr_row)
-    t.gen_text("", t.curr_row, count=120, contin=True)
+    t.gen_typing_text(
+        "\x1b[92m# Have a nice day kind stranger :D Thanks for stopping by!",
+        t.curr_row,
+        contin=True,
+    )
+    t.gen_text("", t.curr_row, count=80, contin=True)
 
     t.gen_gif()
-    # image = gifos.utils.upload_imgbb("output.gif", 129600)  # 1.5 days expiration
-    # print(f"Image URL: {image.url}\nDeletion URL: {image.delete_url}")
+    image = gifos.utils.upload_imgbb("output.gif", 129600)  # 1.5 days expiration
+    print(f"Image URL: {image.url}\nDeletion URL: {image.delete_url}")
 
 
 if __name__ == "__main__":
